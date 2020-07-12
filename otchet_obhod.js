b706f390993710ac46a6f9cb66acb3f583ef992a
Mr-Hei7enberg <63899989+Mr-Hei7enberg@users.noreply.github.com>
2020-07-12 15:38:56 +0300

    Create otchet_obhod.js

---
+++ otchet_obhod.js
@@ -0,0 +1,12 @@
+function otchet_obhod() {
+  var sheet = ss.getSheetByName('обходы');
+  var names = sheet.getRange(2, 1, sheet.getLastRow()-1).getValues().filter(item => item != '');
+  
+  
+  names.forEach((item, index) => {
+                var result = findTechnik(item[0]);
+                sheet.getRange(index + 2, 2).setValue(result[1]);
+                sheet.getRange(index + 2, 3).setValue(result[0].toString().trim());
+                })           
+  
+}
